04240983b066dbb113941a2324be0b15786a2675
Andreas Olsson <basen82@icloud.com>
2024-05-02 16:32:04 +0200

    Fix for scheduler code in table bug.

--- tables/library-data.php
+++ tables/library-data.php
@@ -29,6 +29,8 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/config.php';
 $alla = $_POST['all'];
 $groups = $_POST['groups'];
+$allscheds = $_POST['alls'];
+$scheds = $_POST['scheds'];
 $ausr = $_POST['ausr'];
 $draw = $_POST['draw'];
 $row = $_POST['start'];
@@ -39,73 +41,124 @@ $columnSortOrder = $_POST['order'][0]['dir'];
 $searchValue = $_POST['search']['value'];
 
 $searchArray = array();
-
 $searchQuery = " ";
 if ($searchValue != '') {
-    if ($alla == 1) {
-    $searchQuery = " AND usa.USER_NAME = :usernam AND (grid.NUMBER LIKE :number OR 
+    if ($alla == 1 && $allscheds == 1) {
+        $searchQuery = " AND usa.USER_NAME = :usernam AND (grid.NUMBER LIKE :number OR 
            grid.GROUP_NAME LIKE :group_name OR
            grid.TITLE LIKE :title OR 
            grid.ARTIST LIKE :artist ) ";
-    $searchArray = array(
-        'usernam' => $ausr,
-        'number' => "%$searchValue%",
-        'group_name' => "%$searchValue%",
-        'title' => "%$searchValue%",
-        'artist' => "%$searchValue%"
-    );
-} else {
-    $searchQuery = " AND grid.GROUP_NAME = :groupa AND (grid.NUMBER LIKE :number OR 
+        $searchArray = array(
+            'usernam' => $ausr,
+            'number' => "%$searchValue%",
+            'group_name' => "%$searchValue%",
+            'title' => "%$searchValue%",
+            'artist' => "%$searchValue%"
+        );
+    } else {
+        if ($alla != 1 && $allscheds == 1) {
+            $searchQuery = " AND grid.GROUP_NAME = :groupa AND (grid.NUMBER LIKE :number OR 
            grid.GROUP_NAME LIKE :group_name OR
            grid.TITLE LIKE :title OR 
            grid.ARTIST LIKE :artist ) ";
-    $searchArray = array(
-        'groupa' => $groups,
-        'number' => "%$searchValue%",
-        'group_name' => "%$searchValue%",
-        'title' => "%$searchValue%",
-        'artist' => "%$searchValue%"
-    );
-}
+            $searchArray = array(
+                'groupa' => $groups,
+                'number' => "%$searchValue%",
+                'group_name' => "%$searchValue%",
+                'title' => "%$searchValue%",
+                'artist' => "%$searchValue%"
+            );
+        } else if ($allscheds != 1 && $alla == 1) {
+            $searchQuery = " AND sched.SCHED_CODE = :schedd AND (grid.NUMBER LIKE :number OR 
+           grid.GROUP_NAME LIKE :group_name OR
+           grid.TITLE LIKE :title OR 
+           grid.ARTIST LIKE :artist ) ";
+            $searchArray = array(
+                'schedd' => $scheds,
+                'number' => "%$searchValue%",
+                'group_name' => "%$searchValue%",
+                'title' => "%$searchValue%",
+                'artist' => "%$searchValue%"
+            );
+        } else if ($allscheds != 1 && $alla != 1) {
+            $searchQuery = " AND grid.GROUP_NAME = :groupa AND sched.SCHED_CODE = :schedd AND (grid.NUMBER LIKE :number OR 
+           grid.GROUP_NAME LIKE :group_name OR
+           grid.TITLE LIKE :title OR 
+           grid.ARTIST LIKE :artist ) ";
+            $searchArray = array(
+                'groupa' => $groups,
+                'schedd' => $scheds,
+                'number' => "%$searchValue%",
+                'group_name' => "%$searchValue%",
+                'title' => "%$searchValue%",
+                'artist' => "%$searchValue%"
+            );
+        }
+
+    }
 } else {
 
-    if ($alla == 1) {
+    if ($alla == 1 && $allscheds == 1) {
         $searchQuery = " AND usa.USER_NAME = :usernam ";
         $searchArray = array(
             'usernam' => $ausr
         );
     } else {
-        $searchQuery = " AND grid.GROUP_NAME = :groupa ";
-        $searchArray = array(
-            'groupa' => $groups
-        );
+        if ($alla != 1 && $allscheds == 1) {
+            $searchQuery = " AND grid.GROUP_NAME = :groupa ";
+            $searchArray = array(
+                'groupa' => $groups
+            );
+        } else if ($allscheds != 1 && $alla == 1) {
+            $searchQuery = " AND sched.SCHED_CODE = :schedd ";
+            $searchArray = array(
+                'schedd' => $scheds
+            );
+        } else if ($allscheds != 1 && $alla != 1) {
+            $searchQuery = " AND sched.SCHED_CODE = :schedd AND grid.GROUP_NAME = :groupa ";
+            $searchArray = array(
+                'schedd' => $scheds,
+                'groupa' => $groups
+            );
+        }
+
     }
 
 }
 
-if ($alla == 1) {
-    $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN USER_PERMS usa ON  grid.GROUP_NAME = usa.GROUP_NAME WHERE usa.USER_NAME = :usernam");
+if ($alla == 1 && $allscheds == 1) {
+    $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER LEFT JOIN USER_PERMS usa ON  grid.GROUP_NAME = usa.GROUP_NAME WHERE usa.USER_NAME = :usernam");
     $stmt->execute([':usernam' => $ausr]);
 } else {
-    $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME WHERE grid.GROUP_NAME = :groupname");
-    $stmt->execute([':groupname' => $groups]);
+    if ($alla != 1 && $allscheds == 1) {
+        $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER WHERE grid.GROUP_NAME = :groupname");
+        $stmt->execute([':groupname' => $groups]);
+    } else if ($allscheds != 1 && $alla == 1) {
+        $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER WHERE sched.SCHED_CODE = :schedcode");
+        $stmt->execute([':schedcode' => $scheds]);
+    } else if ($alla != 1 && $allscheds != 1) {
+        $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER WHERE sched.SCHED_CODE = :schedcode AND grid.GROUP_NAME = :groupname");
+        $stmt->execute([':schedcode' => $scheds,
+        ':groupname' => $groups]);
+    }
+
 }
 $records = $stmt->fetch();
 $totalRecords = $records['allcount'];
 
-if ($alla == 1) {
-$stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN USER_PERMS usa ON  grid.GROUP_NAME = usa.GROUP_NAME WHERE 1 " . $searchQuery);
+if ($alla == 1 && $allscheds == 1) {
+    $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER LEFT JOIN USER_PERMS usa ON  grid.GROUP_NAME = usa.GROUP_NAME WHERE 1 " . $searchQuery);
 } else {
-    $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME WHERE 1 " . $searchQuery);    
+    $stmt = $db->prepare("SELECT COUNT(*) AS allcount FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER WHERE 1 " . $searchQuery);
 }
 $stmt->execute($searchArray);
 $records = $stmt->fetch();
 $totalRecordwithFilter = $records['allcount'];
 
-if ($alla == 1) {
-$stmt = $db->prepare("SELECT * FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN USER_PERMS usa ON grid.GROUP_NAME = usa.GROUP_NAME WHERE 1 " . $searchQuery . " ORDER BY " . $columnName . " " . $columnSortOrder . " LIMIT :limit,:offset");
+if ($alla == 1 && $allscheds == 1) {
+    $stmt = $db->prepare("SELECT * FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER LEFT JOIN USER_PERMS usa ON grid.GROUP_NAME = usa.GROUP_NAME WHERE 1 " . $searchQuery . " ORDER BY " . $columnName . " " . $columnSortOrder . " LIMIT :limit,:offset");
 } else {
-    $stmt = $db->prepare("SELECT * FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME WHERE 1 " . $searchQuery . " ORDER BY " . $columnName . " " . $columnSortOrder . " LIMIT :limit,:offset"); 
+    $stmt = $db->prepare("SELECT * FROM CART grid LEFT JOIN GROUPS clk ON grid.GROUP_NAME=clk.NAME LEFT JOIN CART_SCHED_CODES sched ON grid.NUMBER=sched.CART_NUMBER WHERE 1 " . $searchQuery . " ORDER BY " . $columnName . " " . $columnSortOrder . " LIMIT :limit,:offset");
 }
 
 foreach ($searchArray as $key => $search) {
